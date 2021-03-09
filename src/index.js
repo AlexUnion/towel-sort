d659ab0bb9d22a4249d8d0733739b3e452b35546
Al_chemick <alex2015mig@gmail.com>
2021-03-10 00:28:44 +0200

    feat: add towelSort realization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+    let result = [];
+    for (let i = 0; i < matrix.length; i++) {
+        result = result.concat(
+            i % 2 === 0 ?
+                matrix[i] :
+                matrix[i].sort((a, b) => b - a)
+        );
+    }
+    return result;
 }
